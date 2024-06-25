926a21f0a716aacf042349cfe4927a8e72ca608b
Sumit shriwastava <sumit87478@gmail.com>
2024-06-25 19:16:18 +0530

    styling changed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,11 @@ return (
     <h2>Top dishes near you</h2>
     <div className="food-display-list">
       {food_list.map((item,index)=>{
-        return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
+        if(category==="All" || category===item.category){
+          return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
+        }
+
+      
 
       })}
     </div>
